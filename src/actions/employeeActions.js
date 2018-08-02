9f86c4f23ea6703567f69bd1a421ddb0cb832db6
marjorie <marjorie@majojo.local>
2018-08-01 23:06:53 -0700

    complete create employee

--- src/actions/employeeActions.js
+++ src/actions/employeeActions.js
@@ -1,5 +1,9 @@
-import { EMPLOYEE_UPDATE } from "./types";
+import { 
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE
+} from "./types";
 import firebase from "firebase";
+import { Actions } from 'react-native-router-flux';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -11,8 +15,14 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
 
-  firebase
-    .database()
-    .ref(`/users/${currentUser.uid}/employees`)
-    .push({ name, phone, shift });
+  return (dispatch) => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE })
+        Actions.main({ type: 'reset' })
+      });
+  }
 };
